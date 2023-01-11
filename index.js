422b1e57548a3007c46a10a8bc7f10cc52ba6007
Keshav Jha <keskuma2@cisco.com>
2023-01-11 15:01:19 +0530

    "changed file

--- index.js
+++ index.js
@@ -1,15 +1,18 @@
+require('dotenv').config()
 const { Configuration, OpenAIApi } = require("openai");
 const configuration = new Configuration({
-  apiKey: "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",
+  apiKey: process.env.OPENAI_API_KEY,
 });
 const openai = new OpenAIApi(configuration);
 
  async function findallModel(){
+  console.log(configuration.apiKey)
+  //console.log(process.env)
     const response = await openai.listModels();
     console.log("Extracting"+response)
     return response;
 }
 
 findallModel().then(response=>{console.log(response.data)
-console.log(Object.length(response))
+//console.log(Object.length(response))
 }).catch(error=> console.log(error));
